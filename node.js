6eb0796f0874f030cfbbb846ae06193789e56105
voltarou <161274983+voltarou@users.noreply.github.com>
2025-04-22 16:17:54 +0700

    Add files via upload

---
+++ node.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("ini video Somebody Pleasure");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("ini Audio Somebody Pleasure");
+});
+
+gambar.addEventListener("click", function gambar() {
+    alert("ini gambar Somebody Pleasure");
+});
